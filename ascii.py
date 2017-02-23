c11b3bbcef76538a7e33e1a7b643c29b0e1e504f
Morris Green <mogreen@alum.mit.edu>
2017-02-23 13:52:39 -0500

    removed windows OS specific functions

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import binascii
-import sys, msvcrt
+import sys
 
 def decode(str):
     temp_string = binascii.unhexlify(str)
@@ -16,20 +16,11 @@ def encode(str):
     return temp_string
 
 def main():
-    str = "733N1GN3 1A>17T>313"
-    str = input("input string: ")
+    encoded_string = input("input string: ")
     
-    # encoded_string = encode(str)
-    encoded_string = str
-
-    # print("original string: {}".format(encoded_string))
     decoded_string = decode(encoded_string)
 
     print("decoded string: {}".format(decoded_string))
-
-    print("press any key to exit...")
-    msvcrt.getch()
-    sys.exit(0)
 
 if __name__ == "__main__":
     main()
